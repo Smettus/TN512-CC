8de3fdcc2de98c679518ad1cde2cce3c96522d72
Smettus <timdesmet9800@gmail.com>
2024-10-22 12:10:02 +0200

    Frontend

--- visualizeOSM/basic_html/map.js
+++ visualizeOSM/basic_html/map.js
@@ -1,12 +1,91 @@
-// Initialize the map and set its view to a default location
-var map = L.map('map').setView([50.8446, 4.3933], 13);
+// See documentation: https://leafletjs.com/reference.html
 
-// Set up the OpenStreetMap tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+// Setup the map
+var map = L.map('map', {
+    center: [50.8446, 4.3933],
+    zoom: 13,
+    zoomcontrol: true
+}) //.setView([50.8446, 4.3933], 13);
+
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+var satelliteLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+});
+
+osmLayer.addTo(map);
+
+//      - Layer control
+var baseLayers = {
+    "OpenStreetMap": osmLayer,
+    "Satellite": satelliteLayer
+};
+L.control.layers(baseLayers).addTo(map);
+
+
+//      - Component layers (Plane, Ship, Groundtroops)
+// XXX TODO
+// Placeholder geojson
+/*
+https://leafletjs.com/reference.html#geojson
+var geojson_planes
+
+
+var geojsonLayer = L.geoJSON(geojson_planes, {
+    onEachFeature: function (feature, layer) {
+        // Bind a popup to each feature
+        if (feature.properties && feature.properties.name) {
+            layer.bindPopup(feature.properties.name + ': ' + feature.properties.description);
+        }
+    }
 }).addTo(map);
 
-// Check if Geolocation is supported
+
+//https://leafletjs.com/reference.html#marker
+// for adding markers
+*/
+
+
+
+
+// Upon map move -> api call
+function getBoundingBox() {
+    var bounds = map.getBounds();
+    return {
+        southwest: bounds.getSouthWest(),
+        northwest: { lat: bounds.getNorthEast().lat, lng: bounds.getSouthWest().lng },
+        northeast: bounds.getNorthEast(),
+        southeast: { lat: bounds.getSouthWest().lat, lng: bounds.getNorthEast().lng }
+    };
+}
+function fetchPositions() {
+    var bbox = getBoundingBox();
+    var bboxString = `${bbox.southwest.lng},${bbox.southwest.lat},${bbox.northwest.lng},${bbox.northwest.lat},${bbox.northeast.lng},${bbox.northeast.lat},${bbox.southeast.lng},${bbox.southeast.lat}`;
+
+
+    // TODO: how will fetchPositions talk to the backend?
+    //      decide upon format
+    // Make a POST request to the backend
+    console.log(bboxString);
+}
+
+
+map.on('moveend', fetchPositions);
+
+
+
+
+
+
+
+
+
+
+// TODO: add to ask browser location
+/* // Check if Geolocation is supported
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
         function(position) {
@@ -28,4 +107,4 @@ if (navigator.geolocation) {
 } else {
     alert('Geolocation is not supported by this browser.');
 }
-
+ */
