d29e57afa206a4da9c10f745766e1c25ec0964c4
Smettus <timdesmet9800@gmail.com>
2024-12-30 00:21:08 +0100

    custom external mysql port

--- DB_Api_Docker/C2V2/C2V2/settings.py
+++ DB_Api_Docker/C2V2/C2V2/settings.py
@@ -137,7 +137,7 @@ DATABASES = {
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
         'HOST': os.environ.get('MYSQL_HOST'),  # Nom du service Docker, ici 'db'
-        'PORT': os.environ.get('MYSQL_INTERNAL_PORT'),  # Le port de la base de données
+        'PORT': os.environ.get('MYSQL_INTERNAL_PORT'),  # Need the internal port because needs to be 3306 in the docker network
     }
 }
 
